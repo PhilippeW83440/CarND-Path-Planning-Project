1c71a5563144a503244c0ced812aad94427817d1
PhilippeW <philippe.weingertner@gmail.com>
2017-10-16 16:59:05 +0200

    more accurate getXY and getFrenet functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "json.hpp"
 
 #include "spline.h"
+#include <time.h>
 
 using namespace std;
 
@@ -46,18 +47,52 @@ int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vect
 	double closestLen = 100000; //large number
 	int closestWaypoint = 0;
 
-	for(int i = 0; i < maps_x.size(); i++)
-	{
-		double map_x = maps_x[i];
-		double map_y = maps_y[i];
-		double dist = distance(x,y,map_x,map_y);
-		if(dist < closestLen)
-		{
-			closestLen = dist;
-			closestWaypoint = i;
-		}
+  if (maps_x.size() <= 200)
+  {
+	  for(int i = 0; i < maps_x.size(); i++)
+	  {
+	  	double map_x = maps_x[i];
+	  	double map_y = maps_y[i];
+	  	double dist = distance(x,y,map_x,map_y);
+	  	if(dist < closestLen)
+	  	{
+	  		closestLen = dist;
+	  		closestWaypoint = i;
+	  	}
+	  }
+  }
+  else // Faster search with big maps: 2 hierarchical steps of search
+  {
+    // 1) Search a point relatively close to the nearest
+    int jump_points = maps_x.size() / 181; // so that we have 1 jump_points with a 181 points map (default)
+    int point = 0;
+	  while(point < maps_x.size())
+    {
+	  	double map_x = maps_x[point];
+	  	double map_y = maps_y[point];
+	  	double dist = distance(x,y,map_x,map_y);
+	  	if(dist < closestLen)
+	  	{
+	  		closestLen = dist;
+	  		closestWaypoint = point;
+	  	}
+      point += jump_points;
+    }
 
-	}
+    // 2) Search a point which is the nearest in a refined area
+	  //for(int i = closestWaypoint - 181; i < closestWaypoint + 181; i++)
+	  for(int i = closestWaypoint - 91; i < closestWaypoint + 91; i++)
+    {
+	  	double map_x = maps_x[i];
+	  	double map_y = maps_y[i];
+	  	double dist = distance(x,y,map_x,map_y);
+	  	if(dist < closestLen)
+	  	{
+	  		closestLen = dist;
+	  		closestWaypoint = i;
+	  	}
+    }
+  }
 
 	return closestWaypoint;
 
@@ -85,7 +120,7 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y, const vector<double> &maps_s)
 {
 	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
 
@@ -121,12 +156,13 @@ vector<double> getFrenet(double x, double y, double theta, const vector<double> 
 	}
 
 	// calculate s value
-	double frenet_s = 0;
-	for(int i = 0; i < prev_wp; i++)
-	{
-		frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
-	}
+	// XXX double frenet_s = 0;
+	// XXX for(int i = 0; i < prev_wp; i++)
+	// XXX {
+	// XXX 	frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
+	// XXX }
 
+  double frenet_s = maps_s[prev_wp];
 	frenet_s += distance(0,0,proj_x,proj_y);
 
 	return {frenet_s,frenet_d};
@@ -242,20 +278,60 @@ int main() {
     cout << "len_lane" << i << "=" << len_lane << endl;
   }
 
+  // map with higher precision: 1 point every 1 meter (instead of every 30 meters)
+  vector<double> new_map_waypoints_x;
+  vector<double> new_map_waypoints_y;
+  vector<double> new_map_waypoints_dx;
+  vector<double> new_map_waypoints_dy;
+  for (double s = 0; s <= 6945; s++)
+  {
+    double x = spline_x(s);
+    double y = spline_y(s);
+    double dx = spline_dx(s);
+    double dy = spline_dy(s);
+
+    new_map_waypoints_x.push_back(x);
+    new_map_waypoints_y.push_back(y);
+    new_map_waypoints_dx.push_back(dx);
+    new_map_waypoints_dy.push_back(dy);
+  }
+
+	double frenet_s = 0;
+  vector<double> map_s;
+  map_s.push_back(0.0);
+	for(int i = 1; i < map_waypoints_x.size(); i++)
+	{
+		frenet_s += distance(map_waypoints_x[i], map_waypoints_y[i], map_waypoints_x[i-1], map_waypoints_y[i-1]);
+    map_s.push_back(frenet_s);
+	}
+
+	frenet_s = 0;
+  vector<double> new_map_s;
+  new_map_s.push_back(0.0);
+	for(int i = 1; i < new_map_waypoints_x.size(); i++)
+	{
+		frenet_s += distance(new_map_waypoints_x[i], new_map_waypoints_y[i], new_map_waypoints_x[i-1], new_map_waypoints_y[i-1]);
+    //new_map_s.push_back(frenet_s);
+    new_map_s.push_back(i);
+    cout << "frenet_s=" << frenet_s << " " << i << endl;
+	}
+
 
   //////////////////////////////////////////////////////////////////////
   int lane = 1;
   double ref_vel = 0.0; // mph
+  double max_error = 0.0;
   //////////////////////////////////////////////////////////////////////
 
 
-  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_s, &new_map_s, &new_map_waypoints_x, &new_map_waypoints_y, &new_map_waypoints_dx, &new_map_waypoints_dy, &spline_x, &spline_y, &spline_dx, &spline_dy, &max_error,&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     //auto sdata = string(data).substr(0, length);
     //cout << sdata << endl;
+
 
 
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
@@ -289,12 +365,34 @@ int main() {
           	auto sensor_fusion = j[1]["sensor_fusion"];
                 //vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
+            // check transformations accuracy
+            clock_t start = clock();
+            vector<double> frenet = getFrenet(car_x, car_y, car_yaw, new_map_waypoints_x, new_map_waypoints_y, new_map_s);
+            //vector<double> frenet = getFrenet(car_x, car_y, car_yaw, map_waypoints_x, map_waypoints_y, map_s);
+            int frenet_s = frenet[0];
+            int frenet_d = frenet[1];
+
+            double my_x = spline_x(frenet_s) + frenet_d * spline_dx(frenet_s);
+            double my_y = spline_y(frenet_s) + frenet_d * spline_dy(frenet_s);
+            //car_xy = getXY(frenet[0], frenet[1], map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            clock_t stop = clock();
+            double elapsed = (double)(stop - start) * 1000000.0 / CLOCKS_PER_SEC;
+
+            double error = distance(my_x, my_y, car_x, car_y);
+            if (error > max_error)
+            {
+              max_error = error;
+            }
 
           	json msgJson;
 
-            //////////////////////////////////////////////////////////////////////
 
             int prev_size = previous_path_x.size();
+
+            cout << "error=" << error << " trt_time=" << elapsed << " us (max_error=" << max_error << ")" 
+                 << " prev_size=" << prev_size << endl;
+
+            //////////////////////////////////////////////////////////////////////
 
             if (prev_size > 0)
             {
@@ -324,6 +422,12 @@ int main() {
                   // also flag to try to change lane
                   //ref_vel = 29.5; //mph
                   too_close = true;
+                  
+                  //if (lane < 2)
+                  //{
+                  //  lane++;
+                  //}
+
                   if (lane > 0)
                   {
                     lane--;
@@ -409,8 +513,8 @@ int main() {
             }
 
 
-            tk::spline s;
-            s.set_points(ptsx, ptsy);
+            tk::spline spl;
+            spl.set_points(ptsx, ptsy);
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
@@ -423,7 +527,7 @@ int main() {
 
             // Calculate how to break up spline points so that we travel at our desired reference velocity
             double target_x = 30.0;
-            double target_y = s(target_x);
+            double target_y = spl(target_x);
             double target_dist = sqrt(target_x*target_x + target_y*target_y);
 
             double x_add_on = 0;
@@ -434,7 +538,7 @@ int main() {
             {
               double N = (target_dist / (0.02*ref_vel/2.24)); // divide by 2.24: mph -> m/s
               double x_point = x_add_on + target_x/N;
-              double y_point = s(x_point);
+              double y_point = spl(x_point);
 
               x_add_on = x_point;
 
@@ -454,23 +558,6 @@ int main() {
 
             //////////////////////////////////////////////////////////////////////
 
-#if 0
-            cout << "car_s=" << car_s << " card_d=" << car_d << endl;
-            vector<double> frenet;
-            frenet = getFrenet(car_x, car_y, car_yaw, map_waypoints_x, map_waypoints_y);
-            cout << "frenet_s=" << frenet[0] << " frenet_d=" << frenet[1] << endl << endl;
-
-            double dist_inc = 0.3;
-            for (int i = 0; i < 50; i++)
-            {
-              double next_s = car_s + (i+1) * dist_inc;
-              double next_d = 6;
-              vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-
-              next_x_vals.push_back(xy[0]);
-              next_y_vals.push_back(xy[1]);
-            } 
-#endif
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
