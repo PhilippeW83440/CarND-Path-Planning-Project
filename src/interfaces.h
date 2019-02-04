966c9597b34422f9ade96b6c53d3b42617e12f21
AUBERT Sebastien <sebastien.s.aubert@renault.com>
2019-02-04 16:09:30 +0100

    refine IF with control and simulator

--- src/interfaces.h
+++ src/interfaces.h
@@ -7,21 +7,25 @@
 #include "map.h"
 #include "trajectory.h"
 
-struct ItfFusionPlanning {
-  CarData car; // ego 
-  PreviousPath previous_path;
-  std::vector<std::vector<double>> sensor_fusion; // other objects
-
-  Map map;
+struct ItfFusionPlanning { // FUSION -> PLANNING
+  CarData car; // ego: x,y,yaw,norm_v(,s,d)
+  PreviousPath previous_path; // SCANeR => 49 pts, Unity => 8 pts in mean
+  std::vector<std::vector<double>> sensor_fusion; // other objects: car_id,x,y,vx,vy(,s,d)
 };
 
-struct ItfPlanningCtrl {
-  TrajectoryJMT trajectory;
-  double dt;
+struct ItfNavigationPlanning { // NAV -> PLANNING
+  Map map; // x,y,s,dx,dy (SCANeR 1 pt every 1.11m, Unity 1 pt every 30m->1m)
+};
 
-  std::vector<double> poly_s;
-  std::vector<double> poly_d;
-  double tmax;
+struct ItfPlanningCtrl {  // PLANNING -> CTRL
+  TrajectoryJMT trajectory;   // TrajectoryXY, TrajectorySD
+  std::vector<double> poly_s; // TJP
+  std::vector<double> poly_d; // TJP
+  double tmax;                // TJP
+};
+
+struct ItfCtrlPlanning { // CTRL -> PLANNING
+  double dt;
 };
 
 
